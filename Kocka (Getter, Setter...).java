d74be717984585ee6dd9bbe4583d64fd79f35743
ByMikiii <75531503+ByMikiii@users.noreply.github.com>
2022-02-15 10:59:43 +0100

    Update Kocka (Getter, Setter...).java

--- Kocka (Getter, Setter...).java
+++ Kocka (Getter, Setter...).java
@@ -1,14 +1,19 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        Random random = new Random();
         Kocka kockaOsemStranna = new Kocka(8);
         Kocka kockaDesatStranna = new Kocka(10);
 
         System.out.println(kockaOsemStranna.getPocetStien());
         System.out.println(kockaDesatStranna.getPocetStien());
 
+        System.out.println(kockaOsemStranna.hod(10));
+        System.out.println( kockaDesatStranna.hod(10));
 }
 }
 
@@ -37,5 +42,18 @@ public class Kocka {
     //Setter
     public void setPocetStien(int novyPocetStien) {
         pocetStien = novyPocetStien;
+    }
+
+    public int hod(int pocetHodov){
+        int sucetHodov = 0;
+        for (int j = 0; j <pocetHodov; j++ ){
+            int i = random.nextInt(pocetStien);
+            while(i == 0){
+                i = random.nextInt(pocetStien + 1);
+            }
+            sucetHodov = sucetHodov + i;
+        }
+
+        return sucetHodov;
     }
 }
